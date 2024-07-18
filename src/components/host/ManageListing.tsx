ab5161039001ba176018b8f67342ebc7aafbd092
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-07-18 12:17:07 +0530

    added skelton to managelisting ..

--- src/components/host/ManageListing.tsx
+++ src/components/host/ManageListing.tsx
@@ -40,6 +40,7 @@ const ManageListing = () => {
     )
   }
   if (loading) {
+    
     return (
       Array.from({ length: 8 }).map((_, index) => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">>
